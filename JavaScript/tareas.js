71f5f921a0ebc571eecaa84413e761255849f300
Matias Ayesa <matiasayesa99@outlook.com>
2023-05-03 22:16:03 -0300

    .

---
+++ JavaScript/tareas.js
@@ -0,0 +1,182 @@
+// NOTA: "fungi" es el plural de "fungus".
+// DECLARACION DEL ARRAY Y CLASE.
+const fungi = []
+
+class Fungus {
+    constructor(id, nombre, nombreBusqueda, clasificacion, precio, unicelular, letal, stock, img, imgSrc) {
+        this.id = id
+        this.nombre = nombre
+        this.nombreBusqueda = nombreBusqueda
+        this.clasificacion = clasificacion
+        this.precio = precio
+        this.unicelular = unicelular
+        this.letal = letal
+        this.stock = stock
+        this.img = img
+        this.imgSrc = imgSrc
+    }
+}
+
+function newFungus(id, nombre, nombreBusqueda, clasificacion, precio, unicelular, letal, stock, img, imgSrc) {
+    let fungus1 = new Fungus(id, nombre, nombreBusqueda, clasificacion, precio, unicelular, letal, stock, img, imgSrc)
+    fungi.push(fungus1)
+}
+
+newFungus(1, "Levadura", "Levaduras", "levaduras", 50, true, false, 500, "item-a", "../img/levadura.png")
+newFungus(2, "Mazatapec", "mazatapec psilocibina psilocibine alucinogeno", "Basidiomycota", 3500, false, false, 500, "item-b", "../img/mazatapec.png")
+newFungus(3, "Melena de león", "melena de leon lions mane lion's mane", "Basidiomycota", 1383, false, false, 500, "item-c", "../img/melenaDeLeon.png")
+newFungus(4, "Penicilina", "medicinal medicina penicilina", "deuteromycetes", 35000, true, false, 500, "item-d", "../img/penicilina.png")
+newFungus(5, "Amanita Muscaria", "amanita muscaria psilocibina psilocibine alucinogeno", "Basidiomycota", 5500, false, true, 500, "item-e", "../img/amanitaMuscaria.png")
+
+
+//RENDER DE CARDS Y CARRITO
+let carrito = JSON.parse(localStorage.getItem("carrito")) || []
+
+
+// RENDER CARDS
+let capturarDiv = document.getElementById("card__div")
+renderCard(fungi, capturarDiv)
+
+function renderCard(arrayDeFungus, contenedor) {
+    contenedor.innerHTML = ""
+    crearCards = arrayDeFungus.forEach(fungus => {
+        let crearDiv = document.createElement("div")
+        crearDiv.className = "card__fungus"
+        crearDiv.innerHTML = `
+	<div class="row">
+    <div class="col-md-4 col-sm-6 col-xs-12">
+    <div class="card">
+    <div class="cover ${fungus.img}">
+    <h1>${fungus.nombre}</h1>
+    <span class="price">$${fungus.precio}/kg</span>
+    <div class="card-back">
+    <a href="#" id="${fungus.id}" class="anadir__carrito">Añadir al carrito</a>
+    <a href="#" id="" class="detalles">Detalles</a>
+    </div>
+    </div>
+    </div>
+    </div>`
+        contenedor.append(crearDiv)
+        let carritoBoton = document.getElementById(`${fungus.id}`)
+        carritoBoton.onclick = aniadirAlCarrito
+    })
+}
+
+//ADD TO CART N MORE
+function aniadirAlCarrito(e) {
+    e.preventDefault()
+    let localizarFungus = fungi.find(Element => Element.id == e.target.id)
+    if (carrito.some(Element => Element.id == localizarFungus.id)) {
+        let repetido = carrito.findIndex(Element => Element.id == localizarFungus.id)
+        carrito[repetido].kg++
+        carrito[repetido].subtotal = carrito[repetido].precio * carrito[repetido].kg
+    } else {
+        carrito.push({
+            id: localizarFungus.id,
+            nombre: localizarFungus.nombre,
+            precio: localizarFungus.precio,
+            kg: 1,
+            subtotal: localizarFungus.precio,
+            imgSrc: localizarFungus.imgSrc
+        })
+
+    }
+    console.log(carrito)
+    actualizarCarrito()
+}
+
+//RENDER CARRITO Y PRECIO
+let body = document.getElementById("tbody")
+
+function actualizarCarrito() {
+    renderCarrito(carrito)
+    renderSubtotal(carrito)
+
+    //GUARDAR EN LOCAL STORAGE
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+}
+
+function renderCarrito(array) {
+    body.innerHTML = ""
+    array.forEach(Element => {
+        body.innerHTML += `<tr>
+    <td><img src="${Element.imgSrc}" class="img-cart"></td>
+    <td><strong>${Element.nombre}</strong></td>
+    <td>
+    <form class="form-inline">
+        <input class="form-control" type="text" value="${Element.kg}kg">
+        <button rel="tooltip" class="btn btn-default"><i class="fa fa-pencil"></i></button>
+        <a href="#" class="btn btn-primary"><i class="fa fa-trash-o"></i></a>
+    </form>
+    </td>
+    <td>${Element.precio}</td>
+    <td>${Element.subtotal}</td>
+    </tr>`
+})
+}
+
+// RENDER PRECIO
+let precioTotal = document.getElementById("total__price")
+function renderSubtotal(array) {
+    precioTotal.innerHTML = ""
+    let plataAPagar = 0
+array.forEach(Element => {
+    plataAPagar += Element.kg * Element.precio
+})
+precioTotal.innerHTML = `$${plataAPagar}`
+}
+
+actualizarCarrito()
+// // ELIMINAR ARTICULO DEL CARRITO
+// let trash = document.getElementById("trash__can")
+
+// window.onload = function(){ 
+//     trash.onclick = eliminarDelCarrito
+//     function eliminarDelCarrito(e) {
+//         e.preventDefault()
+//     }
+// }
+
+// MOSTRAR Y OCULTAR CARRITO
+let botonCarrito = document.getElementById("boton__carrito")
+let carritoDiv = document.getElementById("carrito__div")
+botonCarrito.onclick = mostrarCarrito
+
+function mostrarCarrito() {
+    if (carritoDiv.classList.contains("hidden")) {
+        carritoDiv.classList.remove("hidden")
+    } else {
+        carritoDiv.classList.add("hidden")
+    }
+}
+
+//BUSCAR ARTICULOS (render con filter)
+let searchInput = document.getElementById("search__input")
+let searchBtn = document.getElementById("search__btn")
+let searchForm = document.getElementById("search__form")
+searchBtn.onclick = filtrarCard
+         
+const accentMap = {
+  'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u'
+}
+    function accent_fold(s) {
+        if (!s) { return '' }
+        let ret = ''
+        for (let i = 0; i < s.length; i++) {
+            ret += accentMap[s.charAt(i)] || s.charAt(i);
+    }
+    return ret
+}  
+
+function filtrarCard(e) {
+      // if (searchInput.value == "") {
+      //     e.preventDefault()
+        //     le t searchError = searchInput.cloneNode(true)
+        //     searchError.id = "search__problemo"
+        //     searchInput.searchForm.replaceChild(searchError, searchInput)
+    // } else
+        e.preventDefault()
+        let filtroAcento = accent_fold(searchInput.value.toLowerCase())
+        let filtrar = fungi.filter(Element => Element.nombreBusqueda.toLowerCase().includes(filtroAcento))
+        renderCard(filtrar, capturarDiv)
+}
